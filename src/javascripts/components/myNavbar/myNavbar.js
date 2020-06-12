1bd37c5fedb99bdd5b937592fe7342602bfdda5a
Michele Rawlins <shellbop@att.net>
2020-06-11 22:38:27 -0500

    added form

--- src/javascripts/components/myNavbar/myNavbar.js
+++ src/javascripts/components/myNavbar/myNavbar.js
@@ -4,7 +4,7 @@ import './myNavbar.scss';
 
 const loadNavbar = () => {
   let domString = '';
-  domString += '<nav class="navbar navbar-expand navbar">';
+  domString += '<nav class="navbar navbar-expand navbar expand="md">';
   domString += '<img src ="https://live.staticflickr.com/65535/49995569386_71eaf52f74_n.jpg" class="navbar logo-icon"></img>';
   domString += '<div class="navbar vb-red">';
   domString += '<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">';
@@ -15,7 +15,7 @@ const loadNavbar = () => {
   domString += '    <a class="nav-item nav-link active" href="#">Home <span class="sr-only">(current)</span></a>';
   domString += '    <a class="nav-item nav-link" href="#">Features</a>';
   domString += '   <a class="nav-item nav-link" href="#">Pricing</a>';
-  domString += '   <a class="nav-item nav-link" href="#">Contact Us</a>';
+  domString += '   <a class="nav-item nav-link" href="#">Request a Quote</a>';
   domString += '  </div>';
   domString += '</div>';
   domString += '</div>';
