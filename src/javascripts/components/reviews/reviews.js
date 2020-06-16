da7d1cdba8026b5434e3cc2fbc772a44a95ae733
Michele Rawlins <shellbop@att.net>
2020-06-16 13:06:21 -0500

    setup complete

--- src/javascripts/components/reviews/reviews.js
+++ src/javascripts/components/reviews/reviews.js
@@ -1,8 +1,27 @@
-const customerReviews = () => {
-  let domString = '';
-  domString += '<div class="card">';
-  domString += '<div class="card-body">';
-  domString += ' This is some text within a card body.';
-  domString += ' </div>';
-  domString += '</div>';
-}
+import utils from '../../../helpers/utils';
+
+
+import reviews from '../../../helpers/data/reviewData';
+
+const createReviewCards = () => {
+  // let domString = '';
+  reviews.getReviews()
+    .then((reviewsList) => {
+      let domString = '<div class="d-flex flex-wrap review container">';
+      console.error('hello', reviewsList);
+      reviewsList.forEach((finishedReviews) => {
+        domString += '<div class="col-sm">';
+        domString += '<div class="card-body review col">';
+        domString += '<div class="row">';
+        domString += `<img class="card-img-top review-screenshot col-sm" src="${finishedReviews.imgUrl}">`;
+        domString += '</div>';
+        domString += '</div>';
+        domString += '<div class="card-footer">';
+        domString += '</div>';
+
+        utils.printToDom('reviews', domString);
+      })
+        .catch((err) => console.error('getReviews broke', err));
+    });
+};
+export default { createReviewCards };
--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -1,9 +1,11 @@
 import navbar from './components/myNavbar/myNavbar';
 import '../styles/main.scss';
+import reviewCards from './components/reviews/reviews';
 
 
 const init = () => {
   navbar.loadNavbar();
+  reviewCards.createReviewCards();
 };
 
 init();
